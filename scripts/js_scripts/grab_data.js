bce6a291fc97960b327035b3f56797c1069e11e6
Michael Christensen <michael.christensen@rpsgroup.com>
2018-05-13 16:12:45 -0700

    efforts to get object from s3

--- scripts/js_scripts/grab_data.js
+++ scripts/js_scripts/grab_data.js
@@ -2,18 +2,23 @@
 // Shorthand for $( document ).ready()
 $(function() {
     console.log( "ready!" );
-    $('#grab-data').click(function(){alert('going to s3 to grab data')});
-    debugger
+    // $('#grab-data').click(function(){alert('going to s3 to grab data')});
+    let filename = 'oceanmapper-data-storage/RTOFS_OC/0m/rtofs_currents_20160512_00.json';
+    let filetype = 'json';
+    getSignedRequest(filename,filetype);
 });
 
-function getSignedRequest(file){
+function getSignedRequest(filename, filetype){
+  debugger
   const xhr = new XMLHttpRequest();
-  xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+  // xhr.open('GET', `/sign-s3?file-name=${filename}&file-type=${filetype}`);
+  xhr.open('GET', `/sign-s3?file-name=${filename}`);
   xhr.onreadystatechange = () => {
     if(xhr.readyState === 4){
       if(xhr.status === 200){
+      	debugger
         const response = JSON.parse(xhr.responseText);
-        uploadFile(file, response.signedRequest, response.url);
+        downloadFile(filename, response.signedRequest, response.url);
       }
       else{
         alert('Could not get signed URL.');
@@ -23,6 +28,25 @@ function getSignedRequest(file){
   xhr.send();
 }
 
+function downloadFile(filename, signedRequest, url){
+  debugger
+  const xhr = new XMLHttpRequest();
+  // xhr.open('GET', signedRequest);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+      	debugger
+        // document.getElementById('preview').src = url;
+        // document.getElementById('avatar-url').value = url;
+      }
+      else{
+        alert('Could not upload file.');
+      }
+    }
+  };
+  xhr.open('GET', signedRequest);
+  xhr.send();
+}
 
 
 // getSignedRequest(file);
--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const express = require('express');
 const aws = require('aws-sdk');
 const path = require('path');
 
+aws.config.region = 'us-east-1';
+const S3_BUCKET = process.env.S3_BUCKET_NAME;
+console.log(S3_BUCKET);
+
 /*
  * Set-up and run the Express app.
  */
@@ -35,6 +39,48 @@ app.get('/', function (req, res) {
 app.get('/about', function (req, res) {
   // res.sendFile(path.join(__dirname + 'views/about.html'));
   console.log(res);
+});
+
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+
+  debugger
+  s3.getObject(
+	  { Bucket: "my-bucket", Key: "my-picture.jpg" },
+	  function (error, data) {
+	    if (error != null) {
+	      alert("Failed to retrieve an object: " + error);
+	    } else {
+	      alert("Loaded " + data.ContentLength + " bytes");
+	      // do something with data.Body
+	    }
+	  }
+	);
+  
+  // const fileName = req.query['file-name'];
+  // const fileType = req.query['file-type'];
+
+  // debugger
+  // const s3Params = {
+  //   Bucket: S3_BUCKET,
+  //   Key: fileName,
+  //   Expires: 60,
+  //   ContentType: fileType,
+  //   ACL: 'public-read'
+  // };
+
+  // s3.getSignedUrl('getObject', s3Params, (err, data) => {
+  //   if(err){
+  //     console.log(err);
+  //     return res.end();
+  //   }
+  //   const returnData = {
+  //     signedRequest: data,
+  //     url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+  //   };
+  //   res.write(JSON.stringify(returnData));
+  //   res.end();
+  // });
 });
 
 
