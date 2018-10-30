ec52acfb8ce70735456ada17cccead951d3f4a64
christensenmichael0 <christensenmichael0@gmail.com>
2018-10-29 17:29:31 -0700

    initially add persistent drawer

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withTheme } from '@material-ui/core/styles';
+// import './App.css';
+import Map from './Map';
+import PersistentDrawerLeft from './components/PersistentDrawerLeft.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <PersistentDrawerLeft />
       </div>
     );
   }
 }
 
-export default App;
+export default withTheme()(App);
+// export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import './index.css';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e0e0e0'
+    },
+    secondary: purple
+  },
+  status: {
+    danger: 'orange',
+  }
+});
+
+function MuiAppContainer() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <App />
+    </MuiThemeProvider>
+  );
+}
+
+ReactDOM.render(<MuiAppContainer />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
