d1f7d06b3ff6cfde6b98607807e5d64da17a556e
christensenmichael0 <christensenmichael0@gmail.com>
2019-01-06 17:43:16 -0500

    include docs in fetch active drilling script

--- scripts/python_scripts/fetch_active_drilling.py
+++ scripts/python_scripts/fetch_active_drilling.py
@@ -1,6 +1,5 @@
 import boto3
-import datetime
-import os
+import json
 import re
 import io
 
@@ -8,85 +7,118 @@ import geopandas
 import requests
 from PyPDF2 import PdfFileReader
 
+AWS_BUCKET_NAME = 'oceanmapper-data-storage'
+DEEPWATER_ACTIVITY_URL = "https://bsee.gov/weeklyreport/deepwater_activity/current_report"
+
 def text_extractor(fileObj):
-  pdf = PdfFileReader(fileObj)
+    """
+    text_extractor(fileObj)
 
-  combined_text = ''
-  for page_indx in range(pdf.getNumPages()):
-    page = pdf.getPage(page_indx)
-    text = page.extractText()
-    header_row_tail = 'Rig Name\n'
-    start_indx = text.index(header_row_tail) + len(header_row_tail)
-    trimmed_text = text[start_indx:]
-    combined_text += trimmed_text
+    Extracts text from a pdf file object using pyPDF2 library
 
-  return combined_text
+    Inputs:
+    fileObj: file object
+    """
+    pdf = PdfFileReader(fileObj)
+    combined_text = ''
+    for page_indx in range(pdf.getNumPages()):
+        page = pdf.getPage(page_indx)
+        text = page.extractText()
+        header_row_tail = 'Rig Name\n'
+        start_indx = text.index(header_row_tail) + len(header_row_tail)
+        trimmed_text = text[start_indx:]
+        combined_text += trimmed_text
 
-file_url = "https://bsee.gov/weeklyreport/deepwater_activity/current_report"
-resp = requests.get(file_url)
+    return combined_text
 
-with io.BytesIO() as pdf:
-  pdf.write(resp.content)
-  raw_text = text_extractor(pdf).replace('\n', '::').replace('\r', '')
-  
-pattern = r'[\w\s.&]+::\w{2}\s+\d*::[\w\d]+::[^:]*::[\d,]*:{0,2}[^:]+' # good!!!
-records = re.findall(pattern, raw_text)
+def lambda_handler(event, context):
+    """
+    lambda_handler(event, context):
 
-# TODO update this when running in aws i.e. /blocks
-block_path = 'blocks/blocks.shp'
-block_path = '/home/mchriste/Desktop/shapefiles/blocks/blocks.shp'
-df_blocks = geopandas.read_file(block_path)
+    This function is used to fetch and parse the current deepwater activity pdf report.
+    Data is saved to s3 bucket as a json file
+    -----------------------------------------------------------------------
+    Inputs:
 
-block_dict = {}
-for index, row in df_blocks.iterrows():
-  try:
-    block_name = row['AC_LAB']
-    block_center = [row.geometry.centroid.coords[0][0],row.geometry.centroid.coords[0][1]] # lon, lat
-    block_dict.setdefault(block_name, block_center)
-  except:
-    print('block failed!!')
-    continue
-
-# various patterns
-depth_pattern = r'[\d,]+'
-
-# loop through records and create some json
-active_drilling_output = []
-for record in records:
-
-  # Assume a record will always contain an operator, block, well_target_lease, rig name
-  try:
-    record_split = record.split('::')
-    operator = record_split[0]
-    block = record_split[1].replace(' ','')
-    well_target_lease = record_split[2]
+    event: AWS Lambda uses this parameter to pass in event data to the handler. 
+    This parameter is usually of the Python dict type. It can also be list, str, int, float, or NoneType type.
     
-    prospect_name = record_split[3] if not re.search(depth_pattern, record_split[3]) else 'n/a'
-    # water depth is second to last
-    water_depth = record_split[-2] if re.search(depth_pattern, record_split[-2]) else 'n/a'
+    context: AWS Lambda uses this parameter to provide runtime information to your handler. 
+    This parameter is of the LambdaContext type.
+    -----------------------------------------------------------------------
+    Output: None
+    -----------------------------------------------------------------------
+    Author: Michael Christensen
+    Date Modified: 01/06/2019
+    """
 
-    # rig name is always last
-    rig_name = record_split[-1] if len(record_split)>=5 else 'n/a'
+    resp = requests.get(DEEPWATER_ACTIVITY_URL)
 
-    #get coordinates of block
-    coords = block_dict.get(block, None)
+    with io.BytesIO() as pdf:
+        pdf.write(resp.content)
+        raw_text = text_extractor(pdf).replace('\n', '::').replace('\r', '')
+      
+    pattern = r'[\w\s.&]+::\w{2}\s+\d*::[\w\d]+::[^:]*::[\d,]*:{0,2}[^:]+' # good!!!
+    records = re.findall(pattern, raw_text)
 
-    drilling_info_obj = {
-      'operator': operator,
-      'block': block,
-      'coordinates': coords or '',
-      'well_target_lease': well_target_lease,
-      'prospect_name': prospect_name,
-      'water_depth': water_depth + '(ft)',
-      'rig_name': rig_name
-    }
+    # update when running locally
+    # block_path = '/home/mchriste/Desktop/shapefiles/blocks/blocks.shp'
+    block_path = 'blocks/blocks.shp'
+    df_blocks = geopandas.read_file(block_path)
 
-    active_drilling_output.append(drilling_info_obj)
-  except:
-    print('in failure block!')
-    print(record)
-    continue
+    block_dict = {}
+    for index, row in df_blocks.iterrows():
+        try:
+            block_name = row['AC_LAB']
+            block_center = [row.geometry.centroid.coords[0][0],row.geometry.centroid.coords[0][1]] # lon, lat
+            block_dict.setdefault(block_name, block_center)
+        except:
+            print('block failed!!')
+            continue
 
-# TODO: create a lambda function
-print('need to save to a json file on s3.. and run on a cron')
+    # various patterns
+    depth_pattern = r'[\d,]+'
 
+    # loop through records and create some json
+    active_drilling_output = []
+    for record in records:
+      # Assume a record will always contain an operator, block, well_target_lease, rig name
+        try:
+            record_split = record.split('::')
+            operator = record_split[0]
+            block = record_split[1].replace(' ','')
+            well_target_lease = record_split[2]
+
+            prospect_name = record_split[3] if not re.search(depth_pattern, record_split[3]) else 'n/a'
+            # water depth is second to last
+            water_depth = record_split[-2] if re.search(depth_pattern, record_split[-2]) else 'n/a'
+
+            # rig name is always last
+            rig_name = record_split[-1] if len(record_split)>=5 else 'n/a'
+
+            #get coordinates of block
+            coords = block_dict.get(block, None)
+
+            drilling_info_obj = {
+              'operator': operator,
+              'block': block,
+              'coordinates': coords or '',
+              'well_target_lease': well_target_lease,
+              'prospect_name': prospect_name,
+              'water_depth': water_depth + '(ft)',
+              'rig_name': rig_name
+            }
+
+            active_drilling_output.append(drilling_info_obj)
+        except:
+            print('in failure block!')
+            print(record)
+            continue
+
+    output_json_path = 'current_deepwater_activity.json'
+    client = boto3.client('s3')
+    client.put_object(Body=json.dumps(active_drilling_output), Bucket=AWS_BUCKET_NAME, Key=output_json_path)
+
+
+if __name__ == '__main__':
+    lambda_handler('','')
--- server/scripts/apiGatewayRouter.js
+++ server/scripts/apiGatewayRouter.js
@@ -7,8 +7,10 @@ const apiGatewayRouter = express.Router();
 // https://stackoverflow.com/questions/39301227/external-api-calls-with-express-node-js-and-require-module
 
 apiGatewayRouter.get('/individual-field', function(req, res, next) {
+  console.log('in the individual-field route!!');
+  console.log(process.env.INDIVIDUAL_FIELD_ENDPOINT);
   request({
-    uri: `${process.env.INDIVIDUAL_FIELD_ENPOINT}`,
+    uri: `${process.env.INDIVIDUAL_FIELD_ENDPOINT}`,
     headers: {
       'x-api-key': process.env.AWS_API_GATEWAY_KEY
     },
