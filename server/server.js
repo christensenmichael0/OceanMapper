d6bf7b3004ce5c57be316339844216bec0a1c14f
christensenmichael0 <christensenmichael0@gmail.com>
2019-03-28 18:22:28 -0400

    add cluster module to improve server performance if resources are available

--- server/server.js
+++ server/server.js
@@ -2,6 +2,8 @@
  * Import required packages.
  * Packages should be installed with "npm install".
  */
+import cluster from 'cluster';
+import os from 'os';
 import bodyParser from 'body-parser';
 import express from 'express';
 import path from 'path';
@@ -11,47 +13,71 @@ import errorHandler from 'errorhandler';
 import downloadFromS3Router from './scripts/downloadFromS3';
 import apiGatewayRouter from './scripts/apiGatewayRouter';
 
-const app = express();
-app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({extended: false}));
+// Code to run if we're in the master process
+if (cluster.isMaster) {
+  // Count the machine's CPUs
+  var cpuCount = os.cpus().length;
+  console.log(`Number of available CPUs: ${cpuCount}`);
 
-const router = express.Router();
-const staticFiles = express.static(path.join(__dirname, '../../client/build'));
-app.use(staticFiles);
+  // Create a worker for each CPU
+  for (var i = 0; i < cpuCount; i += 1) {
+    cluster.fork();
+  }
 
+  // cluster.on('listening', (worker, address) => {
+  //   console.log(
+  //     `A worker is now connected to ${address.address}:${address.port}`);
+  // });
 
-const S3_BUCKET = process.env.S3_BUCKET_NAME;
+  // restart an exited worker
+  cluster.on('exit', (worker, code, signal) => {
+    console.log('worker %d died (%s). restarting...',
+                worker.process.pid, signal || code);
+    cluster.fork();
+  });
 
-// logging middleware 
-app.use(morgan('dev'));
+} else {
+  // worker process
+  const app = express();
+  app.use(bodyParser.json());
+  // app.use(bodyParser.urlencoded({extended: false}));
 
-// add other routes below
-app.use('/download', downloadFromS3Router);
-app.use('/data', apiGatewayRouter);
+  const router = express.Router();
+  const staticFiles = express.static(path.join(__dirname, '../../client/build'));
+  app.use(staticFiles);
 
+  const S3_BUCKET = process.env.S3_BUCKET_NAME;
 
-// for testing
-app.get('/cities', (req, res) => {
-  const cities = [
-    {name: 'New York City', population: 8175133},
-    {name: 'Los Angeles',   population: 3792621},
-    {name: 'Chicago',       population: 2695598}
-  ]
-  res.json(cities)
-});
+  // logging middleware 
+  app.use(morgan('dev'));
 
-// error handler here: (prior middleware should pass next(errorObj))
-app.use(errorHandler());
+  // add other routes below
+  app.use('/download', downloadFromS3Router);
+  app.use('/data', apiGatewayRouter);
 
-// 404 on failed get (doesnt exist)
-// 400 dont do something for some reason
-// 204 sucessful delete
-// 200 sucessful get
+  // for testing
+  app.get('/test', (req, res) => {
+    const cities = [
+      {name: 'New York City', population: 8175133},
+      {name: 'Los Angeles',   population: 3792621},
+      {name: 'Chicago',       population: 2695598}
+    ]
+    res.json(cities)
+  });
 
-app.use('/*', staticFiles)
+  // error handler here: (prior middleware should pass next(errorObj))
+  app.use(errorHandler());
 
-app.set('port', (process.env.PORT || 3001))
+  // 404 on failed get (doesnt exist)
+  // 400 dont do something for some reason
+  // 204 sucessful delete
+  // 200 sucessful get
 
-app.listen(app.get('port'), () => {
-  console.log(`Listening on ${app.get('port')}`)
-})
+  app.use('/*', staticFiles)
+
+  app.set('port', (process.env.PORT || 3001))
+
+  app.listen(app.get('port'), () => {
+    console.log(`Worker ${cluster.worker.id} listening on ${app.get('port')}`)
+  })
+}
